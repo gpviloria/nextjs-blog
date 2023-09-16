502f213f9508a470975cc790c16bbb7eec11de40
gpviloria <141867341+gpviloria@users.noreply.github.com>
2023-09-16 23:30:31 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,16 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <h2><b>10 Things That Require Zero Talent</b></h2>
-        <p>	1. Being On Time</p>
-        <p>	2. Making An Effort</p>
-        <p>	3. Being High Energy</p>
-        <p>	4. Having A Positive Attitude</p> 
-        <p>	5. Being Passionate</p>
-        <p>	6. Using Good Body Language</p>
-        <p>	7. Being Coachable</p>
-        <p>	8. Doing A Little Extra</p> 
-        <p>	9. Being Prepared</p>
-        <p>	10. Having A Strong Work Ethic</p>
+        <p> •	1. Being On Time</p>
+        <p> •	2. Making An Effort</p>
+        <p> •	3. Being High Energy</p>
+        <p> •	4. Having A Positive Attitude</p> 
+        <p>	•	5. Being Passionate</p>
+        <p>	•	6. Using Good Body Language</p>
+        <p>	•	7. Being Coachable</p>
+        <p>	•	8. Doing A Little Extra</p> 
+        <p>	•	9. Being Prepared</p>
+        <p>	•	10. Having A Strong Work Ethic</p>
         <br>
         <p>
           This is a sample website using:{' '}

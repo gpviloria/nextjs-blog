ec77641c0aa760530a67e1009b7621f891ea33c3
gpviloria <141867341+gpviloria@users.noreply.github.com>
2023-09-16 23:25:08 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,25 +11,15 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <h2><b>10 Things That Require Zero Talent</b></h2>
         <p>•	1. Being On Time</p>
-        <br>
         <p>•	2. Making An Effort</p>
-        <br>
         <p>•	3. Being High Energy</p>
-        <br>
         <p>•	4. Having A Positive Attitude</p> 
-        <br>
         <p>•	5. Being Passionate</p>
-        <br>
         <p>•	6. Using Good Body Language</p>
-        <br>
         <p>•	7. Being Coachable</p>
-        <br>
         <p>•	8. Doing A Little Extra</p> 
-        <br>
         <p>•	9. Being Prepared</p>
-        <br>
         <p>•	10. Having A Strong Work Ethic</p>
-        <br>
         <p>
           This is a sample website using:{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.

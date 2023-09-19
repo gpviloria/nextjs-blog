d602b00d23e63018a623fd59f7c8a7189f807b05
gpviloria <141867341+gpviloria@users.noreply.github.com>
2023-09-19 17:04:06 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,17 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <h2><b>Manifesto for Agile Software Development</b></h2>
+        <h2>Manifesto for Agile Software Development</h2>
       
-        <p> We are uncovering better ways of developingt software by doing it and helping otgher do it.
+        <p> 
+          We are uncovering better ways of developingt software by doing it and helping otgher do it.
           Through this work we have come to value:
         </p>
-        <p>Individuls and interactions over process and tools Working software over 
+        <p>
+          Individuls and interactions over process and tools Working software over 
           comprehensive documentation Customer collaboartion over contract negotiation
-            Responding to change over following a plan</p>
+            Responding to change over following a plan
+              </p>
         <p>
           That is, while there is value in the items on the right, we value the items on the left more. 
         </p>
